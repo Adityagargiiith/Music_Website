49f0133860165571ad63bdd85e0797a3972a689c
Garvit <garvitgupta955@gmail.com>
2023-05-08 16:04:27 +0530

    final phase3

--- JS/post_songspage.js
+++ JS/post_songspage.js
@@ -1,7 +1,11 @@
-
 function add(event) {
+        var button = event.target;
+        button.disabled = true;
+        button.textContent = "Added to Playlist";
+        
         const row = event.target.parentNode.parentNode;
         var title = row.querySelector(".title").textContent;
+        title = title.substring(2);
         var duration = row.querySelector(".duration").textContent;
         var artistname=document.querySelector(".artist").textContent;
       
--- PYTHON AND SQL,DATABASE/ app.py
+++ PYTHON AND SQL,DATABASE/ app.py
@@ -33,5 +33,32 @@ def add_to_playlist():
 
     return  '', 201
 
+@app.route('/songs', methods=['GET'])
+def get_songs():
+    conn = sqlite3.connect('music.db')
+    c = conn.cursor()
+    c.execute("SELECT id, name, artistname, duration FROM songs")
+    rows = c.fetchall()
+    songs = []
+    for row in rows:
+        songs.append({
+            'id': row[0],
+            'songname': row[1],
+            'artistname': row[2],
+            'duration': row[3]
+        })
+    conn.close()
+    return jsonify({'songs': songs})
+
+@app.route('/songs/<int:id>', methods=['DELETE'])
+def delete_song(id):
+    conn = sqlite3.connect('music.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM songs WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return jsonify({'success': True})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
